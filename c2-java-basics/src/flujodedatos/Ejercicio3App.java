b34527158cc084275d0bfdf9d1b4552676562535
Ivette Margalló <ivettemdv@gmail.com>
2025-02-17 13:48:02 +0100

    n

--- c2-java-basics/src/flujodedatos/Ejercicio3App.java
+++ c2-java-basics/src/flujodedatos/Ejercicio3App.java
@@ -1,12 +1,14 @@
 package flujodedatos;
 
+import javax.swing.JOptionPane;
+
 public class Ejercicio3App {
 
 	public static void main(String[] args) {
-		String a= "Fernando";
-		//Declaro una variable string
+		String a= JOptionPane.showInputDialog("Introduce tu nombre: ");
+		//Declaro una variable string, que se abrirá en un panel editable.
 		
-		System.out.println("Bienvenido "+a);
+		JOptionPane.showMessageDialog(null, "Bien, tu nombre es "+a+", bienvenido/a "+a);
 		
 	}
 
