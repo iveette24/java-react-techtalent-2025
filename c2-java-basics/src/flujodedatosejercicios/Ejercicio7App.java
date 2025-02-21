4d6f9197a91f1c7fa6b579e3a9a2c1efa98dd866
Ivette Margalló <ivettemdv@gmail.com>
2025-02-21 11:02:15 +0100

    for

---
+++ c2-java-basics/src/flujodedatosejercicios/Ejercicio7App.java
@@ -0,0 +1,19 @@
+package flujodedatosejercicios;
+
+public class Ejercicio7App {
+
+	public static void main(String[] args) {
+		//Defino una variable
+		int num=1;
+		
+		//Hago el programa mientras num sea igual o menor a 100
+		
+		while (num<=100) {
+			System.out.println(num);//Se muestra el valor de num
+			num++; //Incrementa 1
+		}
+		
+		
+	}
+
+}
---
+++ c2-java-basics/src/flujodedatosejercicios/Ejercicio8App.java
@@ -0,0 +1,13 @@
+package flujodedatosejercicios;
+
+public class Ejercicio8App {
+
+	public static void main(String[] args) {
+		// Defino una variable
+		// for (inicialización; condición; incremento/decremento);
+		for (int num = 1; num <= 100; num++){
+			System.out.println(num);
+
+		}
+	}
+}
