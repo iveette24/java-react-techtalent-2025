7a5da7860716f87b46b09fa2ead20acceba7b208
Ivette Margalló <ivettemdv@gmail.com>
2025-02-25 12:14:35 +0100

    extras

--- c2-java-basics/src/flujodedatosejercicios/EjercicioExtra4App.java
+++ c2-java-basics/src/flujodedatosejercicios/EjercicioExtra4App.java
@@ -5,8 +5,6 @@ import java.util.Scanner;
 public class EjercicioExtra4App {
 
 	public static void main(String[] args) {
-		// Comptador de vocals en una paraula
-//Escriu un programa que demani una paraula i compti quantes vocals té (a, e, i, o, u).
 
 		// Creamos un scanner para saber la palabra
 		Scanner sc = new Scanner(System.in);
@@ -14,17 +12,21 @@ public class EjercicioExtra4App {
 		String palabra = sc.nextLine();
 		palabra = palabra.toLowerCase();
 		// Lo convertimos a minúsculas para que no haya problemas con las mayúsculas
-		sc.close();
 
 		int contador = 0;
 		// Creamos un contador para saber cuántas vocales hay
+
+		// for (inicialización, condición, incremento o decremento)
 		for (int i = 0; i < palabra.length(); i++) {
 			char letra = palabra.charAt(i);
 			// Creamos una variable char para saber qué letra es
-			if (letra == 'a' || letra == 'e' || letra == 'i' || letra == 'o' || letra == 'u') {
+			// en la posición i de la palabra, que es una cadena de texto
+			if (letra == 'a' || letra == 'e' || letra == 'i' || letra == 'o' || letra == 'u' || letra == 'á'
+					|| letra == 'é' || letra == 'í' || letra == 'ó' || letra == 'ú') {
 				contador++;
 			}
 		}
-
+		System.out.println("Hay " + contador + " vocales en la palabra " + palabra.toUpperCase());
+		sc.close();
 	}
 }
