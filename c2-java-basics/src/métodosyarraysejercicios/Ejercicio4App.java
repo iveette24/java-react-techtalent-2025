9a4a0eed3dfaa4ecc7e961e6c9449f294e35e9d1
Ivette Margalló <ivettemdv@gmail.com>
2025-02-28 13:53:36 +0100

    nuevos ejercicios de método

---
+++ c2-java-basics/src/métodosyarraysejercicios/Ejercicio4App.java
@@ -0,0 +1,22 @@
+package métodosyarraysejercicios;
+
+import javax.swing.JOptionPane;
+
+public class Ejercicio4App {
+
+	public static void main(String[] args) {
+		int num = Integer.parseInt(JOptionPane.showInputDialog("Escribe un número: "));
+
+		int fact = factorial(num);
+		JOptionPane.showMessageDialog(null, "El factorial de " + num + " es: " + fact);
+	}
+
+	public static int factorial(int num) {
+		int fact = 1;
+
+		for (int i = num; i >= 1; i--) {
+			fact *= i;
+		}
+		return fact;
+	}
+}
