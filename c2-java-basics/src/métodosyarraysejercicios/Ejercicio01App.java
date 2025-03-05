81f649990ba6e3248e5bcf845331436597669c5f
Ivette Margalló <ivettemdv@gmail.com>
2025-03-05 14:01:03 +0100

    creados

---
+++ c2-java-basics/src/métodosyarraysejercicios/Ejercicio01App.java
@@ -0,0 +1,54 @@
+package métodosyarraysejercicios;
+
+import javax.swing.JOptionPane;
+
+public class Ejercicio01App {
+
+	public static void main(String[] args) {
+		// Creado por mi
+
+		boolean continuar = true;
+
+		while (continuar) {
+
+			String figura = JOptionPane.showInputDialog(
+					"Introduce" + " el número de la figura: \n 1. Círculo \n " + "2. Triángulo \n 3. Cuadrado");
+
+			switch (figura) {
+			case "1":
+				double radio = Double.parseDouble(JOptionPane.showInputDialog("Introduce el radio: "));
+				JOptionPane.showMessageDialog(null, "El área del círculo es: " + circulo(radio));
+				continuar = false;
+				break;
+
+			case "2":
+				double base = Double.parseDouble(JOptionPane.showInputDialog("Introduce la base: "));
+				double altura = Double.parseDouble(JOptionPane.showInputDialog("Introduce la altura: "));
+				JOptionPane.showMessageDialog(null, "El área del triángulo es: " + triangulo(base, altura));
+				continuar = false;
+				break;
+
+			case "3":
+				double lado = Double.parseDouble(JOptionPane.showInputDialog("Introduce el lado: "));
+				JOptionPane.showMessageDialog(null, "El área del cuadrado es: " + cuadrado(lado));
+				continuar = false;
+				break;
+			default:
+				JOptionPane.showMessageDialog(null, "El valor introducido no es válido");
+			}
+		}
+	}
+
+	public static double circulo(double radio) {
+		return (Math.pow(radio, 2) * Math.PI);
+	}
+
+	public static double triangulo(double base, double altura) {
+		return ((base * altura) / 2);
+	}
+
+	public static double cuadrado(double lado) {
+		return (lado * lado);
+	}
+
+}
---
+++ c2-java-basics/src/métodosyarraysejercicios/Ejercicio08App.java
@@ -0,0 +1,25 @@
+
+package métodosyarraysejercicios;
+
+import javax.swing.JOptionPane;
+
+public class Ejercicio08App {
+
+	public static void main(String[] args) {
+		double[] num = new double[10];
+		rellenarNumeros(num);
+		mostrarNumeros(num);
+	}
+
+	public static void rellenarNumeros(double[] num) {
+		for (int i = 0; i < num.length; i++) {
+			num[i] = Double.parseDouble(JOptionPane.showInputDialog("Introduce el valor de la posición " + (i + 1) + ": "));
+		}
+	}
+
+	public static void mostrarNumeros(double[] num) {
+		for (int i = 0; i < num.length; i++) {
+			System.out.println("Valor " + (i + 1) + ": " + num[i] + "\n");
+		}
+	}
+}
