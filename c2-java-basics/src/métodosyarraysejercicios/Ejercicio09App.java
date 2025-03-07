82b97682c246c00195acb5f38f4a3e00aa07eb4c
Ivette Margalló <ivettemdv@gmail.com>
2025-03-07 14:03:34 +0100

    fatal

--- c2-java-basics/src/métodosyarraysejercicios/Ejercicio09App.java
+++ c2-java-basics/src/métodosyarraysejercicios/Ejercicio09App.java
@@ -6,20 +6,18 @@ import javax.swing.JOptionPane;
 public class Ejercicio09App {
 
 	public static void main(String[] args) {
-		double[] num = new double[10];
-		rellenarNumeros(num);
-		mostrarNumeros(num);
+		int cantidad = Integer.toString(JOptionPane.showInputDialog("Escribe el tamaño del array: "));
+		int array[] = new int[cantidad];
+
 	}
 
-	public static void rellenarNumeros(double[] num) {
-		for (int i = 0; i < num.length; i++) {
-			num[i] = Double.parseDouble(JOptionPane.showInputDialog("Introduce el valor de la posición " + (i + 1) + ": "));
-		}
-	}
+	public static int[] rellenarArray(int cantidad, int[] array) {
+		int random[] = new int[cantidad];
+//	(int) ((Math.random()*9)+1)
+		int arrayrand[] = new int[cantidad];
 
-	public static void mostrarNumeros(double[] num) {
-		for (int i = 0; i < num.length; i++) {
-			System.out.println("Valor " + (i + 1) + ": " + num[i] + "\n");
+		for (int i = 0; i < cantidad; i++) {
+
 		}
 	}
 }
--- c2-java-basics/src/métodosyarraysejercicios/Ejercicio12App.java
+++ c2-java-basics/src/métodosyarraysejercicios/Ejercicio12App.java
@@ -1,5 +1,7 @@
 package métodosyarraysejercicios;
 
+import java.util.Arrays;
+
 import javax.swing.JOptionPane;
 
 public class Ejercicio12App {
@@ -7,10 +9,22 @@ public class Ejercicio12App {
 	public static void main(String[] args) {
 		int dimension = Integer.parseInt(JOptionPane.showInputDialog("Introduce la dimensión del array: "));
 		int[] array1 = new int[dimension];
-
+		// Ninguna acción
 		array1 = randomArray1(array1, dimension);
 
+		// int[] array1 = randomArray1(dimension);
+
 		int numero = Integer.parseInt(JOptionPane.showInputDialog("Introduce el último valor que quieres: "));
+
+		int[] array2 = new int[0];
+		array2 = comparador(array1, numero);
+
+		System.out.println("Los numeros que acaban en " + numero + " son : " + Arrays.toString(array2));
+
+//		comparador(array1, numero);
+		// System.out.println(comparador(array1, numero));
+
+//		System.out.println("estos son los que acaban en " + numero + " : " + array1[i]);
 
 	}
 
@@ -18,21 +32,26 @@ public class Ejercicio12App {
 
 		// Crear un método que haga que los arrays sean creados random
 		array1 = new int[dimension];
+		if (dimension <= 0) {
+			JOptionPane.showMessageDialog(null, "La dimensión del array no es válida.");
 
-		for (int i = 0; i < dimension; i++) {
-			array1[i] = (int) (Math.random() * 300) + 1;
+		} else {
+
+			for (int i = 0; i < dimension; i++) {
+				array1[i] = (int) (Math.random() * 300) + 1;
+			}
 		}
 		return array1;
 	}
 
 	public static int[] comparador(int[] array1, int numero) {
-		int ultimonumero = array1[array1.length - 1];
-		if (ultimonumero == numero) {
-			System.out.println("Los valores ");
-		}
+		int[] resultados = new int[array1.length];
 
 		for (int i = 0; i < array1.length; i++) {
+			resultados[i] = array1[i] % 10;
+
 		}
 
+		return resultados;
 	}
 }
