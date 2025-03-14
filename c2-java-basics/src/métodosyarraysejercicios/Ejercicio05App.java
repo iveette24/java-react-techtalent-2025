92d9ae55a65a2fc42d6cacdf34c9b65f9bcb71ff
Ivette Margalló <ivettemdv@gmail.com>
2025-03-14 12:20:50 +0100

    borrar los innecesarios

--- c2-java-basics/src/métodosyarraysejercicios/Ejercicio05App.java
+++ c2-java-basics/src/métodosyarraysejercicios/Ejercicio05App.java
@@ -6,23 +6,26 @@ import javax.swing.JOptionPane;
 public class Ejercicio05App {
 
 	public static void main(String[] args) {
+		// Hecho por mi
+
 		int num = Integer.parseInt(JOptionPane.showInputDialog("Escribe un número: "));
 
-		String binario = decimalABinario(num);
-		JOptionPane.showMessageDialog(null, "El número " + num + " en binario es: " + binario);
+		String binario = DecimalBinario(num);
+		JOptionPane.showMessageDialog(null, "El número binario de " + num + " es: " + binario);
 	}
 
-	public static String decimalABinario(int num) {
+	public static String DecimalBinario(int num) {
 		if (num == 0) {
 			return "0";
 		}
 
 		StringBuilder binario = new StringBuilder();
 		while (num > 0) {
-			int resto = num % 2;
-			binario.insert(0, resto);
+			int residuo = num % 2;
+			binario.insert(0, residuo); // Insertamos el residuo en el principio
 			num /= 2;
 		}
-		return binario.toString();
+		return binario.toString(); // Covierte el método StringBuilder a String para que pueda ser devuelto por el
+									// método
 	}
 }
--- c2-java-basics/src/métodosyarraysejercicios/Ejercicio06App.java
+++ c2-java-basics/src/métodosyarraysejercicios/Ejercicio06App.java
@@ -4,27 +4,31 @@ package métodosyarraysejercicios;
 import javax.swing.JOptionPane;
 
 public class Ejercicio06App {
+	// Hecho por mi
 
 	public static void main(String[] args) {
-		int num = Integer.parseInt(JOptionPane.showInputDialog("Escribe un número entero positivo: "));
+		int num = Integer.parseInt(JOptionPane.showInputDialog("Escribe un número positivo: "));
 		// Se solicita al usuario que ingrese un número entero positivo mediante un
 		// cuadro de diálogo.
-
-		if (num > 0) {
-			int cifras = numeroDeCifras(num); // Se llama al método numeroDeCifras que convierte el número a una cadena
-												// y devuelve la longitud de la cadena, que corresponde al número de
-												// cifras.
-			JOptionPane.showMessageDialog(null, "El número de cifras de " + num + " es: " + cifras);
-		} else {
-			JOptionPane.showMessageDialog(null, "Por favor, ingrese un número entero positivo.");
+		if (num < 0) {
+			JOptionPane.showMessageDialog(null, "El número introducido no es positivo.");
 			// Se verifica que el número ingresado sea positivo.
+
+		} else {
+			int cifras = NumeroDeCifras(num);
+			JOptionPane.showMessageDialog(null, "El número de cifras de " + num + " es: " + cifras);
+			// Se llama al método numeroDeCifras que convierte el número a una cadena
+			// y devuelve la longitud de la cadena, que corresponde al número de
+			// cifras.
 
 		}
 	}
 
-	public static int numeroDeCifras(int num) {
+	public static int NumeroDeCifras(int num) {
 		return Integer.toString(num).length();
 		// Convierte el número a una cadena y devuelve la longitud de la cadena, que
 		// corresponde al número de cifras.
+
 	}
+
 }
