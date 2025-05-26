18be46670d234aa1771da2ec3337aff972e1e635
Ivette Margalló <ivettemdv@gmail.com>
2025-05-26 11:07:42 +0200

    actualizacion

--- spring-mvc-juniotgn/src/main/java/com/example/mvc/HomeController.java
+++ spring-mvc-juniotgn/src/main/java/com/example/mvc/HomeController.java
@@ -1,16 +1,27 @@
-package com.example;
+package com.example.mvc;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.ui.Model;
 
 @Controller
 public class HomeController {
+	
 
     @RequestMapping(value = "/", method = RequestMethod.GET)
     public String showHomePage() {
         return "hello-world";
     }
+    
+	@RequestMapping(value = "/hello-world", method = RequestMethod.GET)
+    public String showRedirect() {
+        return "redirect:/"; // Redirect to the root URL";
+    }
+
+    
+    @RequestMapping(value = "/servicios", method = RequestMethod.GET)
+	public String showHelloPage() {
+		return "servicios";
+	}
+    
 }
