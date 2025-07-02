943837565b55ab94631fd71570b298739878279a
Ivette Margalló <ivettemdv@gmail.com>
2025-07-02 10:43:17 +0200

    mejora videoclub

---
+++ T22_Ejercicio1_VideoClubMejoraCopiada/src/main/java/AppMain/App.java
@@ -0,0 +1,12 @@
+package AppMain;
+
+import views.formCliente;
+
+public class App
+
+{
+	public static void main(String[] args) {
+		formCliente objetoGestor = new formCliente();
+		objetoGestor.setVisible(true);
+	}
+}
