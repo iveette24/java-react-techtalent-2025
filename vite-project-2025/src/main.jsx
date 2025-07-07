75a54fa8f8e78f18cd9e5a5fb9cf9aadea765f96
Ivette Margalló <ivettemdv@gmail.com>
2025-07-07 12:21:43 +0200

    finiquitado

--- vite-project-2025/src/main.jsx
+++ vite-project-2025/src/main.jsx
@@ -1,10 +1,23 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.jsx'
+// FETCH API
+const apiKey = "vuIT3Mk786renTFwK69IlfK6NbkB09js";
+const peticion = fetch(`http://api.giphy.com/v1/gifs/random?api_key=${apiKey}`);
 
-createRoot(document.getElementById('root')).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
-)
+// peticion.then( resp => console.log(resp))
+// .catch (console.warn);
+
+// peticion.then ( resp => {
+//   resp.json().then( data => {
+//     console.log(data);
+//   })
+// })
+// .catch (console.warn);
+
+peticion
+  .then((resp) => resp.json())
+  .then(({ data }) => {
+    const { url } = data.images.original;
+    const img = document.createElement("img");
+    img.src = url;
+    document.body.append(img);
+  })
+  .catch(console.warn);
